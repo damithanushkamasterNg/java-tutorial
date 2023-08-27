fbbd958d32bdc69d8973d2c3214ea4361620e533
Damith Anushka <damithanushkam016@gmail.com>
2023-08-27 16:41:21 +0530

    Add first package

---
+++ java-tutorial/java_tutorial/src/java_tutorial/Java_tutorial.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_tutorial;
+
+import lession_1.DataType;
+
+/**
+ *
+ * @author Damith
+ */
+public class Java_tutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new DataType();
+    }
+    
+}
---
+++ java-tutorial/java_tutorial/src/lession_1/DataType.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lession_1;
+
+/**
+ *
+ * @author Damith
+ */
+public class DataType {
+
+    int age = 42;
+    double pi = 3.14;
+    char grade = 'A';
+    String greeting = "Hello, Java!";
+    boolean isActive = true;
+
+    public DataType() {
+        System.out.println("age: " + age);
+    }
+}
